9ce28663d8021717f003128cebf2ebc5514eaf97
manuel <feliperiosg@gmail.com>
2021-02-17 00:02:05 +0100

    floating.point in arithmetic ops. bash

--- toast.sh
+++ toast.sh
@@ -9,7 +9,7 @@ echo $(( a**b ))
 echo "b/a:" $(echo "$b/$a" | bc -l)
 echo "a/b without leading zero:" $(echo "$a/$b" | bc -l)
 echo "a/b without leading zero plus %.3f:" $(echo "scale=3; $a/$b" | bc -l)
-echo "b/a with leading zero plus %.7f:" $(printf '%.7f' $(echo "scale=3; $b/$a" | bc -l))
+echo "a/b with leading zero plus %.5f:" $(printf '%.5f' $(echo "$a/$b" | bc -l))
 # space
 echo "last 1 test:" $(printf '%.6f' $(echo "5.01/11.37" | bc -l))
 echo "last 2 test:" $(echo "scale=6; 11.37/5.01" | bc -l)
