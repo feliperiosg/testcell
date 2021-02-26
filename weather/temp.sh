ec70e32f27ac535cb7cf671ba1cf4a42b8c2f5ad
filipo <feliperiosg@gmail.com>
2021-02-26 22:52:31 +0100

    weather www

--- weather/temp.sh
+++ weather/temp.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# https://weather.com/weather/today/l/50.08,14.44
+www=https://weather.com/weather/today/l/50.08,14.44
 
-www=google.com
+# www=google.com
+# www=https://www.accuweather.com/en/cz/dejvice/125514/hourly-weather-forecast/125514
 content=$(wget $www -O "html.txt")
 #content=$(wget google.com -q -O -)
 #echo $content
