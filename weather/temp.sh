1d868165e209679b7295c0125b5015cd7620a575
filipo <feliperiosg@gmail.com>
2021-03-05 23:13:13 +0100

    new failed intent

--- weather/temp.sh
+++ weather/temp.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-www=https://weather.com/weather/today/l/50.08,14.44
-
-# www=google.com
-# www=https://www.accuweather.com/en/cz/dejvice/125514/hourly-weather-forecast/125514
+#www=https://weather.com/weather/today/l/50.08,14.44
+#www=https://www.google.com/
+www=https://demoqa.com/
+#www=https://www.accuweather.com/en/cz/dejvice/125514/hourly-weather-forecast/125514
 content=$(wget $www -O "html.txt")
 #content=$(wget google.com -q -O -)
+
 #echo $content
-less -FX html.txt
+#less -FX html.txt
+
+grep -Po '(?<=href=")[^"]*' "html.txt"
